f913e694177a2794c0f5a2d74e671808ede1e638
Carlo Denaro <818568+blackout314@users.noreply.github.com>
2022-10-20 14:21:16 +0200

    Create flydb.php

---
+++ flydb.php
@@ -0,0 +1,14 @@
+<?php
+$datadb = 'data.json';
+
+$data = json_decode(file_get_contents($datadb), true);
+
+if($_GET['var'] && $_GET['dat']) {
+	$var = basename($_GET['var']);
+    $dat = basename($_GET['dat']);
+	$data[ $var ] = $dat;
+    file_put_contents($datadb, json_encode($data));
+} else {
+	header('Content-Type: application/json');
+	echo json_encode($data);
+}
